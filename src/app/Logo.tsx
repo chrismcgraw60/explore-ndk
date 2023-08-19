430e18a526f32229c286e4bba77089e59af79e1c
Chris McGraw <chrismcgraw60@gmail.com>
2023-08-19 09:20:51 +0100

    refactor: extract componentsout of nav header

---
+++ src/app/Logo.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+
+export default function Logo() {
+    return (
+      <>
+        <div className="flex-shrink-0">
+            <img
+            className="h-8 w-8"
+            src="nostr_logo_blk_prpl.png"
+            alt="Explore-Nostr"
+            />
+        </div>
+      </>
+)}
--- src/app/NavHeader.tsx
+++ src/app/NavHeader.tsx
@@ -3,6 +3,9 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import UserProfile from './UserProfile'
+import Notify from './Notify'
+import Logo from './Logo'
 
 const user = {
   name: 'chrismcgraw60',
@@ -35,13 +38,9 @@ export default function NavHeader() {
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="nostr_logo_blk_prpl.png"
-                        alt="Explore-Nostr"
-                      />
-                    </div>
+
+                    <Logo/>
+
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
@@ -61,57 +60,16 @@ export default function NavHeader() {
                         ))}
                       </div>
                     </div>
+
                   </div>
+
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                      <Notify/>
+                      <UserProfile/>
                     </div>
                   </div>
+
                   <div className="-mr-2 flex md:hidden">
                     {/* Mobile menu button */}
                     <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -125,6 +83,7 @@ export default function NavHeader() {
                     </Disclosure.Button>
                   </div>
                 </div>
+
               </div>
 
               <Disclosure.Panel className="md:hidden">
@@ -144,6 +103,7 @@ export default function NavHeader() {
                     </Disclosure.Button>
                   ))}
                 </div>
+
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
