29191331e436901d91acdfb04b406bb1e4509c35
Chris McGraw <chrismcgraw60@gmail.com>
2023-08-19 15:14:45 +0100

    feat: link to hime via nostr logo

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -4,11 +4,13 @@ export default function Logo() {
     return (
       <>
         <div className="flex-shrink-0">
+          <a href="/" rel="noreferrer">
             <img
-            className="h-8 w-8"
-            src="nostr_logo_blk_prpl.png"
-            alt="Explore-Nostr"
+              className="h-8 w-8"
+              src="nostr_logo_blk_prpl.png"
+              alt="Explore-Nostr"
             />
+          </a>
         </div>
       </>
 )}
