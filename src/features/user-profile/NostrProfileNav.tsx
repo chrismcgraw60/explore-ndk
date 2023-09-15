520bb3e113626d6d936c0f826d59af571af41e12
Chris McGraw <chrismcgraw60@gmail.com>
2023-09-15 16:31:59 +0100

    feat : implement sign in button

--- src/features/user-profile/NostrProfileNav.tsx
+++ src/features/user-profile/NostrProfileNav.tsx
@@ -27,36 +27,29 @@ export default function NostrProfileNav() {
     }
   }
 
-  return (
-    <>
-      {/* Profile dropdown */}
-      <Menu as="div" className="relative ml-3">
-        <div>
-          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span className="absolute -inset-1.5" />
-            <span className="sr-only">Open user menu</span>
-            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+  function profileDropDown() {
+    return (
+      <>
+        {/* Profile dropdown */}
+        <Menu as="div" className="relative ml-3">
+          <div>
+            <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">Open user menu</span>
+              <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 
-            {!userProfiles.npub &&
-              <Menu.Item key="sign_in">
-                { ({ active }) => (<a href="#" onClick={() => connectExtension()} className={menuItemClass(active)}>Sign in</a>) }
-              </Menu.Item>
-            }
-
-            {userProfiles.npub &&
-              <>
               <Menu.Item key="show_profile">
                 { ({ active }) => (<a href="/profile" className={menuItemClass(active)}>Your Profile</a>) }
               </Menu.Item>
@@ -64,13 +57,22 @@ export default function NostrProfileNav() {
               <Menu.Item key="sign_out">
                 { ({ active }) => (<a href="#" onClick={() => userProfiles.clear()} className={menuItemClass(active)}>Sign out</a>) }
               </Menu.Item>
-              </>
-            }
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </>
-  );
+
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </>
+    );
+  }
+
+  function signInButton() {
+    return (
+      <a href="#" onClick={() => connectExtension()} 
+        className="'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'">Sign in</a>
+    );
+  }
+
+  return userProfiles.npub ? profileDropDown() : signInButton();
 }
 
 function menuItemClass(active: boolean): string | undefined {
